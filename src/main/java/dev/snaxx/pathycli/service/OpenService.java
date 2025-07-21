43b528020f2c60301d3172d8b2c3364fcb45fcf8
David <david.snaxx@gmail.com>
2025-07-21 04:30:33 -0400

    Added functions for opening executables by either a provided absolute file path or from an alias mapping if one is present.

--- src/main/java/dev/snaxx/pathycli/service/OpenService.java
+++ src/main/java/dev/snaxx/pathycli/service/OpenService.java
@@ -1,14 +1,62 @@
 package dev.snaxx.pathycli.service;
 
+import dev.snaxx.pathycli.json.PersistenceReader;
+import dev.snaxx.pathycli.model.AliasMapping;
+import dev.snaxx.pathycli.util.PathyUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 public class OpenService {
 
     public OpenService() {}
 
+    public int openExecutable(String request) {
+        PersistenceReader persistenceReader = new PersistenceReader();
+        Optional<AliasMapping> potentialAlias = persistenceReader.getAliasByKey(request);
+
+        // see if we were given a file path instead of an alias
+        if (potentialAlias.isEmpty()) {
+            return openExecutableFromAbsolutePath(request);
+        }
+
+        // look for valid alias
+        AliasMapping alias = potentialAlias.get();
+        return openExecutableFromAliasMapping(alias);
+    }
+
+    /**
+     * Launches the executable file specified by the provided {@link AliasMapping}.
+     * @param alias The alias mapping object containing the relative file path.
+     * @return An {@link ExitCode} for operation success/failure.
+     */
+    public int openExecutableFromAliasMapping(AliasMapping alias) {
+        // validate this is an executable
+        if (!alias.getFileType().equals(".exe")) {
+            return ExitCode.INVALID_ARGUMENT.code();
+        }
+
+        // try to open the executable
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", PathyUtils.resolvePlatformPath(alias).toString());
+        try {
+            pb.inheritIO().start();
+        } catch (IOException ioException) {
+            return ExitCode.FILE_NOT_FOUND.code();
+        }
+
+        return ExitCode.SUCCESS.code();
+    }
+
+    /**
+     * Launches the executable file provided at the given absolute file path.
+     * Use {@link #openExecutableFromAliasMapping(AliasMapping)} if only a relative path is known.
+     * @param request The absolute file path leading to the executable.
+     * @return An {@link ExitCode} for operation success/failure.
+     */
     public int openExecutableFromAbsolutePath(String request) {
+        // validate this is an executable
         if (!request.toLowerCase().endsWith(".exe")) {
             return ExitCode.INVALID_ARGUMENT.code();
         }
@@ -17,6 +65,7 @@ public class OpenService {
             return ExitCode.FILE_NOT_FOUND.code();
         }
 
+        // try to open the executable
         ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", request);
         try {
             pb.inheritIO().start();
--- src/main/java/dev/snaxx/pathycli/util/PathyUtils.java
+++ src/main/java/dev/snaxx/pathycli/util/PathyUtils.java
@@ -1,8 +1,55 @@
 package dev.snaxx.pathycli.util;
 
+import dev.snaxx.pathycli.model.AliasMapping;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * The PathyUtils class contains various utility classes that may/may not be moved to a more relevant class.
+ */
 public class PathyUtils {
 
+    /**
+     * @return The dot extension of the given file path. (ex. ".exe" not "exe")
+     */
     public static String getFileExtension(String filePath) {
         return filePath.substring(filePath.lastIndexOf("."));
     }
+
+    /**
+     * Attempts to mutate the stored path into a full system path regardless of operating system and, for windows,
+     * drive letter.
+     * @param alias The alias to inspect the path of.
+     * @return An absolute file path leading to the given aliased file.
+     */
+    public static Path resolvePlatformPath(AliasMapping alias) {
+        Path relativePath = Paths.get(alias.getRelativePath());
+        if (isWindows()) {
+            // drive letter is not stored, so just loop through the possibilities
+            for (File root : File.listRoots()) {
+                Path withRoot = root.toPath().resolve(relativePath);
+                if (Files.exists(withRoot)) {
+                    return withRoot;
+                }
+            }
+            return relativePath.toAbsolutePath(); // no drive letter found
+        } else { // unix
+            if (relativePath.isAbsolute()) {
+                return relativePath;
+            }
+            return Paths.get(System.getProperty("user.home")).resolve(relativePath).normalize();
+        }
+    }
+
+    /**
+     * @return {@code TRUE} If this is a Windows computer. {@code FALSE} If not.
+     */
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().startsWith("windows");
+    }
 }
+
+
